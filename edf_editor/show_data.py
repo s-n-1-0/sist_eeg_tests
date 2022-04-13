3ebb7ea163f32355737ac0de88faafeb9c7c9845
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2022-04-13 13:05:59 +0900

    edfプロパティを表示

--- edf_editor/show_data.py
+++ edf_editor/show_data.py
@@ -1,9 +1,20 @@
 #%%
 import pyedflib
 import matplotlib.pyplot as plt
+from tabulate import tabulate
 EDF_PATH = "../edf_files/test2_0412_1.edf"
 edf = pyedflib.EdfReader(EDF_PATH)
 
+# %% edfプロパティを表示
+headers = ["Prop", "Value"]
+signalHeader = edf.getSignalHeader(0)
+props = {
+    "Ch Size":f"{len(edf.getSignalLabels())}ch",
+    "Data Size":edf.getNSamples()[0],
+    "Sample Rate":f"{signalHeader['sample_rate']}hz",
+    "time [DataSize/SampleRate]":f"{edf.getFileDuration()}s"
+}
+print(tabulate(props.items(),headers,tablefmt="grid",colalign=('center','center')))
 # %% edf波形データを表示
 labels = edf.getSignalLabels()
 plt.figure(figsize=(7,len(labels)*2))
