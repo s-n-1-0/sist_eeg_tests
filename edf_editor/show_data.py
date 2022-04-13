c10355f677810ea66641594daf2b730ca0f10c3c
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2022-04-13 18:11:03 +0900

    edfファイルのeeg波形をスペクトログラム表示

--- edf_editor/show_data.py
+++ edf_editor/show_data.py
@@ -1,11 +1,14 @@
 #%%
 import pyedflib
 import matplotlib.pyplot as plt
+import matplotlib.mlab as mlab
 import numpy as np
 from tabulate import tabulate
 EDF_PATH = "../edf_files/test2_0412_1.edf"
 edf = pyedflib.EdfReader(EDF_PATH)
-
+ewavs = []
+for idx,label in enumerate(edf.getSignalLabels()):
+    ewavs.append(edf.readSignal(idx))
 # %% edfプロパティを表示
 headers = ["Prop", "Value"]
 signalHeader = edf.getSignalHeader(0)
@@ -23,7 +26,17 @@ times = np.arange(0,edf.getNSamples()[0]) / signalHeader['sample_rate']
 plt.figure(figsize=(7,len(labels)*2))
 for idx,label in enumerate(labels):
     plt.subplot(len(labels),1,idx + 1)
-    plt.plot(times,edf.readSignal(idx),label=labels[idx])
+    plt.plot(times,ewavs[idx],label=labels[idx])
 plt.xlabel("Time(s)")
+plt.show()
+# %% 単純なスペクトログラム
+spec,freqs,t,*_ = plt.specgram(ewavs[0], Fs=250, Fc=0, detrend=mlab.detrend_none, window=mlab.window_hanning, noverlap=128, cmap=None, xextent=None, pad_to=None, sides='default',  scale_by_freq=None, mode='default', scale='default')
+spec = 10 * np.log10(spec)
+#30hz>フィルター
+newFreqsLastIdx = np.where(freqs<=30)[-1][-1]
+newFreqs = freqs[:newFreqsLastIdx + 1]
+print(newFreqs)
+plt.figure()
+plt.pcolormesh(t,newFreqs,spec[:newFreqsLastIdx + 1,:], shading='auto')
 plt.show()
 # %%
