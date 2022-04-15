2fb1df76c272705d812a47c68e2c3af239ca6725
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2022-04-16 00:01:41 +0900

    選択したチャンネルの平均α波、平均β波、平均α/β比を出力

--- edf_editor/show_data.py
+++ edf_editor/show_data.py
@@ -1,10 +1,11 @@
 #%%
 import pyedflib
 import matplotlib.pyplot as plt
+import matplotlib.mlab as mlab
 import numpy as np
 from scipy import signal
 from tabulate import tabulate
-EDF_PATH = "../edf_files/test2_0412_1.edf"
+EDF_PATH = "../edf_files/test2_0412_2.edf"
 CH_IDX = 0
 
 edf = pyedflib.EdfReader(EDF_PATH)
@@ -65,19 +66,31 @@ cutf_beta = np.where((freqs > 13) & (freqs<=25))
 alpha_freqs = freqs[cutf_alpha[0][0]:cutf_alpha[-1][-1] + 1]
 beta_feqs = freqs[cutf_beta[0][0]:cutf_beta[-1][-1] + 1]
 print(especs.shape)
-alpha_power = np.mean(especs[0,cutf_alpha[0][0]:cutf_alpha[-1][-1] + 1,cutt[0][0]:cutt[-1][-1]+1],axis=0)
-beta_power = np.mean(especs[0,cutf_beta[0][0]:cutf_beta[-1][-1] + 1,cutt[0][0]:cutt[-1][-1]+1],axis=0)
-#alphaAvgPower = np.mean(especs[:,cutf_alpha[0][0]:cutf_alpha[-1][-1] + 1,:],axis=(0,1))
-#betaAvgPower = np.mean(especs[:,cutf_beta[0][0]:cutf_beta[-1][-1] + 1,:],axis=(0,1))
-plt.plot(new_time,alpha_power,label="α")
-plt.plot(new_time,beta_power,label="β")
-plt.plot(new_time, beta_power / alpha_power,label="β/α")
+alpha_power = np.mean(especs[CH_IDX,cutf_alpha[0][0]:cutf_alpha[-1][-1] + 1,cutt[0][0]:cutt[-1][-1]+1],axis=0)
+beta_power = np.mean(especs[CH_IDX,cutf_beta[0][0]:cutf_beta[-1][-1] + 1,cutt[0][0]:cutt[-1][-1]+1],axis=0)
+alpha_avg_power = np.mean(especs[:,cutf_alpha[0][0]:cutf_alpha[-1][-1] + 1,cutt[0][0]:cutt[-1][-1]+1],axis=(0,1))
+beta_avg_power = np.mean(especs[:,cutf_beta[0][0]:cutf_beta[-1][-1] + 1,cutt[0][0]:cutt[-1][-1]+1],axis=(0,1))
+plt.title("avg ch : α,β,β/α")
+plt.plot(new_time,alpha_avg_power,label="α")
+plt.plot(new_time,beta_avg_power,label="β")
+plt.plot(new_time, beta_avg_power / alpha_avg_power,label="β/α")
 plt.xlabel("Time(s)")
 plt.legend()
 #plt.ylim(10,-10)
 plt.show()
-print(f"α:{np.mean(alpha_power)}")
-print(f"β:{np.mean(beta_power)}")
-print(f"β/:{np.mean(beta_power/alpha_power)}")
-
+props = {
+    "Avg ch・α":f"{np.mean(alpha_avg_power)}",
+    "Avg ch・β":f"{np.mean(beta_avg_power)}",
+    "Avg ch・β/α":f"{np.mean(beta_avg_power/alpha_avg_power)}"
+}
+print("---Avg ch---")
+print(tabulate(props.items(),["Type","Value"],tablefmt="grid",colalign=('center','center')))
+props = {
+    f"{labels[CH_IDX]}ch・α":f"{np.mean(alpha_power)}",
+    f"{labels[CH_IDX]}ch・β":f"{np.mean(beta_power)}",
+    f"{labels[CH_IDX]}ch・β/α":f"{np.mean(beta_power/alpha_power)}"
+}
+print("\n\n")
+print(f"---{labels[CH_IDX]}ch---")
+print(tabulate(props.items(),["Type","Value"],tablefmt="grid",colalign=('center','center')))
 # %%
