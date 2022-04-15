d91629f6eaafc080baae32b094b614473193d3ad
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2022-04-15 14:57:08 +0900

    show_data.py全体で使いまわす部分をimportセルに移動

--- edf_editor/show_data.py
+++ edf_editor/show_data.py
@@ -13,18 +13,22 @@ edf = pyedflib.EdfReader(EDF_PATH)
 ewavs = []
 for idx,label in enumerate(edf.getSignalLabels()):
     ewavs.append(edf.readSignal(idx))
+def getSpecgram(wav):
+    return 10 * np.log10(np.abs(signal.stft(wav,fs=250, detrend=False, window='hanning', noverlap=128)[2]))
+freqs,t,_ = signal.stft(ewavs[0],fs=250, detrend=False, window='hanning', noverlap=128)
+especs = np.array(list(map(getSpecgram,ewavs)))
+labels = edf.getSignalLabels()
 # %% edfプロパティを表示
 headers = ["Prop", "Value"]
 signalHeader = edf.getSignalHeader(0)
 props = {
-    "Ch Size":f"{len(edf.getSignalLabels())}ch",
+    "Ch Size":f"{len(labels)}ch",
     "Data Size":edf.getNSamples()[0],
     "Sample Rate":f"{signalHeader['sample_rate']}hz",
     "time [DataSize/SampleRate]":f"{edf.getFileDuration()}s"
 }
 print(tabulate(props.items(),headers,tablefmt="grid",colalign=('center','center')))
 # %% edf波形データを表示
-labels = edf.getSignalLabels()
 signalHeader = edf.getSignalHeader(0)
 times = np.arange(0,edf.getNSamples()[0]) / signalHeader['sample_rate']
 plt.figure(figsize=(7,len(labels)*2))
@@ -34,11 +38,6 @@ for idx,label in enumerate(labels):
 plt.xlabel("Time(s)")
 plt.show()
 # %% 単純なスペクトログラム
-def getSpecgram(wav):
-    return 10 * np.log10(np.abs(signal.stft(wav,fs=250, detrend=False, window='hanning', noverlap=128)[2]))
-freqs,t,_ = signal.stft(ewavs[0],fs=250, detrend=False, window='hanning', noverlap=128)
-especs = np.array(list(map(getSpecgram,ewavs)))
-labels = edf.getSignalLabels()
 #30hz>フィルター
 cutf = np.where((freqs >= 0) & (freqs<=30))
 newFreqsLastIdx = cutf[-1][-1]
