32b4a7631285cd000ee32b52ced0befb958601cb
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2022-04-19 17:31:23 +0900

    アノテーション時刻に縦線が引かれたスペクトログラムを表示

--- edf_editor/show_marker.py
+++ edf_editor/show_marker.py
@@ -1,10 +1,12 @@
 #%%
+import lib.spec as spec
+import lib.edf as myedf
 import math
 import pyedflib
+from scipy import signal
 import matplotlib.pyplot as plt
 import matplotlib.mlab as mlab
 import numpy as np
-from scipy import signal
 from tabulate import tabulate
 EDF_PATH = "../edf_files/test3_0419.edf"
 CH_IDX = 0
@@ -15,6 +17,7 @@ for idx,label in enumerate(edf.getSignalLabels()):
     ewavs.append(edf.readSignal(idx))
 labels = edf.getSignalLabels()
 annotaitons = edf.readAnnotations()
+fs = myedf.get_fs(edf)
 # %%
 signal_header = edf.getSignalHeader(0)
 ewav_time = edf.getFileDuration()
@@ -26,4 +29,15 @@ for ai in range(len(annotaitons[0])):
     idx = math.floor(annotaitons[0][ai] * fs)
     rows.append((name,time,idx))
 print(tabulate(rows,["Name","Time(s)","Index"],tablefmt="grid",colalign=('center','center')))
+# %% 【前のセル実行必須】平均波形にアノテーションを表示します。
+freqs,t,_ = signal.stft(ewavs[0],fs=fs, detrend=False, window='hanning', noverlap=128)
+especs = spec.get_spectrograms(ewavs,fs)
+log_especs = 10 * np.log10(especs)
+plt.figure()
+plt.title("avg ch")
+plt.pcolormesh(t,freqs,np.mean(log_especs,axis=0), shading='auto')
+for _,t,_ in rows:
+    plt.vlines(t, 0, freqs[-1],colors='#FF4C4C',linestyle='dashed', linewidth=3)
+plt.show()
+plt.show()
 # %%
