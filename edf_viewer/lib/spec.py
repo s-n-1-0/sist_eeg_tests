bfa0da6d377616acf2f8a45559dc900dc379beec
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2022-04-28 17:58:17 +0900

    オーバーラップサンプル数を指定できるように修正

--- edf_viewer/lib/spec.py
+++ edf_viewer/lib/spec.py
@@ -1,17 +1,17 @@
 from scipy import signal
 import numpy as np
-def get_spectrogram(wav,fs):
+def get_spectrogram(wav,fs,noverlap=128):
     """
     指定した波形のスペクトログラムを求めます。(dB値ではありません)
     """
-    result = np.abs(signal.stft(wav,fs=fs, detrend=False, window='hanning', noverlap=128)[2])
+    result = np.abs(signal.stft(wav,fs=fs, detrend=False, window='hanning', noverlap=noverlap)[2])
     return result
-def get_spectrograms(wavs:list,fs:int):
+def get_spectrograms(wavs:list,fs:int,noverlap=128):
     """
     指定した複数波形をまとめてスペクトログラムに変換します。(dB値ではありません)
     """
     
     def map_get_spectrogram(wav):
-        return get_spectrogram(wav,fs)
+        return get_spectrogram(wav,fs,noverlap=noverlap)
     result = map(map_get_spectrogram,wavs)
     return np.array(list(result))
