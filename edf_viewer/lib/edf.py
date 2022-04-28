3ebf26c5134383e665a6203b53ea69eb612cb512
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2022-04-28 15:38:29 +0900

    アノテーション取得処理をedf_viewerライブラリへ分離(get_annotations)

--- edf_viewer/lib/edf.py
+++ edf_viewer/lib/edf.py
@@ -1,4 +1,6 @@
 import pyedflib
+import math
+import edf_viewer 
 def get_all_signals(edf:pyedflib.EdfReader):
     """
     edfファイルから全チャンネルの波形を取得して返します。
@@ -13,3 +15,14 @@ def get_fs(edf:pyedflib.EdfReader):
     edfファイルからサンプリングレートを取得します。
     """
     return edf.getSignalHeader(0)['sample_rate'] # = fs
+
+def get_annotations(edf:pyedflib.EdfReader):
+    """
+    edfファイルからアノテーション情報を取得します。
+    Returns:
+       タプル(名前,経過秒,間隔,インデックス)を配列で返します。
+    """
+    annotations = edf.readAnnotations()
+    fs = edf_viewer.get_fs(edf)
+    rows:list[tuple[str,float,Any,int]] = [(name,time,duration,math.floor(time * fs))for time,duration,name in zip(annotations[0],annotations[1],annotations[2])]
+    return rows
--- edf_viewer/show_marker.py
+++ edf_viewer/show_marker.py
@@ -1,4 +1,5 @@
 #%%
+import edf_viewer
 import lib.spec as spec
 import lib.edf as myedf
 import math
@@ -16,19 +17,13 @@ ewavs = []
 for idx,label in enumerate(edf.getSignalLabels()):
     ewavs.append(edf.readSignal(idx))
 labels = edf.getSignalLabels()
-annotaitons = edf.readAnnotations()
 fs = myedf.get_fs(edf)
 # %%
 signal_header = edf.getSignalHeader(0)
 ewav_time = edf.getFileDuration()
-rows = []
-fs = signal_header['sample_rate']
-for ai in range(len(annotaitons[0])):
-    name = annotaitons[2][ai]
-    time = annotaitons[0][ai]
-    idx = math.floor(annotaitons[0][ai] * fs)
-    rows.append((name,time,idx))
-print(tabulate(rows,["Name","Time(s)","Index"],tablefmt="grid",colalign=('center','center')))
+annotations = edf_viewer.get_annotations(edf)
+view_annotations = [(name,time,index) for name,time,_,index in annotations]
+print(tabulate(view_annotations,["Name","Time(s)","Index"],tablefmt="grid",colalign=('center','center')))
 # %% 【前のセル実行必須】平均波形にアノテーションを表示します。
 freqs,t,_ = signal.stft(ewavs[0],fs=fs, detrend=False, window='hanning', noverlap=128)
 especs = spec.get_spectrograms(ewavs,fs)
@@ -36,7 +31,7 @@ log_especs = 10 * np.log10(especs)
 plt.figure()
 plt.title("avg ch")
 plt.pcolormesh(t,freqs,np.mean(log_especs,axis=0), shading='auto')
-for _,r,_ in rows:
+for _,r,*_ in annotations:
     plt.vlines(r, 0, freqs[-1],colors='#FF4C4C',linestyle='dashed', linewidth=3)
 plt.show()
 plt.show()
