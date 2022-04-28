2e9e65a861aeeba054376d277588c598af4f1218
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2022-04-28 18:23:39 +0900

    get_spectrogram(s)にnp.arrayしないオプションとnpersegを追加

--- edf_viewer/lib/spec.py
+++ edf_viewer/lib/spec.py
@@ -1,17 +1,21 @@
 from scipy import signal
 import numpy as np
-def get_spectrogram(wav,fs,noverlap=128):
+from sqlalchemy import true
+def get_spectrogram(wav,fs,nperseg=None,noverlap=128):
     """
     指定した波形のスペクトログラムを求めます。(dB値ではありません)
     """
-    result = np.abs(signal.stft(wav,fs=fs, detrend=False, window='hanning', noverlap=noverlap)[2])
+    result = np.abs(signal.stft(wav,fs=fs,nperseg=nperseg,detrend=False, window='hanning', noverlap=noverlap)[2])
     return result
-def get_spectrograms(wavs:list,fs:int,noverlap=128):
+def get_spectrograms(wavs:list,fs:int,nperseg=None,noverlap=128,conv_array=True):
     """
     指定した複数波形をまとめてスペクトログラムに変換します。(dB値ではありません)
     """
     
     def map_get_spectrogram(wav):
-        return get_spectrogram(wav,fs,noverlap=noverlap)
+        return get_spectrogram(wav,fs,nperseg=nperseg,noverlap=noverlap)
     result = map(map_get_spectrogram,wavs)
-    return np.array(list(result))
+    if conv_array:
+        return np.array(list(result))
+    else:
+        return list(result)
