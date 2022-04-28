980917c0526371daba29ee53842af1ec4f723c2a
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2022-04-28 16:16:13 +0900

    edfから実験部分(syncアノテーション以降)を抽出 ※まだ後ろ側は切り捨てしていない

--- tests/pos_neg/src/show_data.py
+++ tests/pos_neg/src/show_data.py
@@ -5,12 +5,25 @@ import numpy as np
 from PIL import Image
 from matplotlib import pyplot as plt
 from itertools import groupby
+import pyedflib
+import edf_viewer
+from edf_viewer import spec
+import pandas as pd
+from datetime import datetime
+import math
+from scipy import signal
 # %% config
+EDF_PATH = "edf_files/pos_neg/posneg_0421_1.edf"
+CSV_PATH = "tests/pos_neg/pos_neg--2022-04-21--16_14_07.csv"
 IMG_DIR_PATH = "tests/pos_neg/images"
 POS_GROUP_NAMES = ["B","P","Q"]
 NEG_GROUP_NAMES = ["A","H","Z"]
 NEU_GROUP_NAMES = ["N"]
-# %% 使用データの表示
+# %% edfファイルの取得
+edf = pyedflib.EdfReader(EDF_PATH)
+fs = edf_viewer.get_fs(edf)#サンプリングレート
+annos = edf_viewer.get_annotations(edf)
+# %% 画像データの表示
 files = glob.glob(f"{IMG_DIR_PATH}/*.bmp")
 def get_file_data(f:str):
     ex = re.search(r"[^\\]+$",f).group()
@@ -41,4 +54,19 @@ exs_groups = [(key,list(group)) for key, group in groupby(exs, key=lambda m: m[2
 plot_groups("Positive Images",exs_groups,POS_GROUP_NAMES)
 plot_groups("Negative Images",exs_groups,NEG_GROUP_NAMES)
 plot_groups("Neutral Images",exs_groups,NEU_GROUP_NAMES)
+# %% edfからeegを取得し実験部分以外を切り捨て
+all_signals = edf_viewer.get_all_signals(edf)
+#use_colsによって実験開始までの行データをスキップ
+working_range_csv = pd.read_csv(CSV_PATH,skiprows=4,usecols=[0,3,7,8,11,12]).values
+#シークエンスタイトルを削除
+working_range_csv = working_range_csv[np.where(working_range_csv[:,0] == 'goodbad')]
+run_times = working_range_csv[:,4] / 1000 #秒に変換
+end_times = working_range_csv[:,5] / 1000 #秒に変換
+#開始アノテーションを0秒とした値
+start_anno = annos[1] # = 'sync'アノテーション
+offset_run_times = np.asarray([rt - run_times[0] for rt in run_times])
+offset_run_time_indexes = [math.floor(ort * fs) for ort in offset_run_times]
+fixed_offset_run_times = np.asarray(offset_run_times) + start_anno[1]
+fixed_offset_run_time_indexes = np.asarray(offset_run_time_indexes) + start_anno[3]
+
 # %%
