4b26bc076375e2b3cfb40aed6d1a7a401c867770
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2022-04-29 13:01:47 +0900

    周波数間隔や時間間隔の取得をpass_rangeで行うように置き換え

--- edf_viewer/show_data.py
+++ edf_viewer/show_data.py
@@ -7,6 +7,7 @@ import matplotlib.mlab as mlab
 import numpy as np
 from scipy import signal
 from tabulate import tabulate
+from edf_viewer import pass_range
 EDF_PATH = "edf_files/test2_0412_2.edf"
 CH_IDX = 0
 
@@ -38,37 +39,29 @@ plt.show()
 # %% 単純なスペクトログラム
 log_especs = 10 * np.log10(especs)
 #30hz>フィルター
-cutf = np.where((freqs >= 0) & (freqs<=30))
-new_freqs_last_idx = cutf[-1][-1]
-new_freqs_first_idx = cutf[0][0]
-new_freqs = freqs[new_freqs_first_idx:new_freqs_last_idx + 1]
-
+cutf,new_freqs = pass_range.pass_range(freqs,0,30)
 plt.figure()
 plt.title("avg ch")
 plt.pcolormesh(t,freqs,np.mean(log_especs,axis=0), shading='auto')
 plt.show()
 plt.title(f"avg ch")
-plt.pcolormesh(t,new_freqs,np.mean(log_especs[:,new_freqs_first_idx:new_freqs_last_idx + 1,:],axis=0), shading='auto')
+plt.pcolormesh(t,new_freqs,np.mean(log_especs[:,cutf,:],axis=0), shading='auto')
 plt.show()
 plt.figure()
 plt.title(f"{labels[CH_IDX]}ch")
 _ = plt.specgram(ewavs[CH_IDX], Fs=fs, Fc=0, detrend=mlab.detrend_none, window=mlab.window_hanning, noverlap=128, cmap=None, xextent=None, pad_to=None, sides='default',  scale_by_freq=None, mode='default', scale='default')
 plt.figure()
 plt.title(f"{labels[CH_IDX]}ch")
-plt.pcolormesh(t,new_freqs,log_especs[CH_IDX,new_freqs_first_idx:new_freqs_last_idx + 1,:], shading='auto')
+plt.pcolormesh(t,new_freqs,log_especs[CH_IDX,cutf,:], shading='auto')
 plt.show()
 # %%  α波、β波グラフをプロット
-cutt = np.where((t>=0) & (t<60)) #指定した範囲で集計
-new_time = t[cutt[0][0]:cutt[-1][-1]+1]
-cutf_alpha = np.where((freqs >= 8) & (freqs<=13))
-cutf_beta = np.where((freqs > 13) & (freqs<=25))
-alpha_freqs = freqs[cutf_alpha[0][0]:cutf_alpha[-1][-1] + 1]
-beta_feqs = freqs[cutf_beta[0][0]:cutf_beta[-1][-1] + 1]
-print(especs.shape)
-alpha_power = np.mean(especs[CH_IDX,cutf_alpha[0][0]:cutf_alpha[-1][-1] + 1,cutt[0][0]:cutt[-1][-1]+1],axis=0)
-beta_power = np.mean(especs[CH_IDX,cutf_beta[0][0]:cutf_beta[-1][-1] + 1,cutt[0][0]:cutt[-1][-1]+1],axis=0)
-alpha_avg_power = np.mean(especs[:,cutf_alpha[0][0]:cutf_alpha[-1][-1] + 1,cutt[0][0]:cutt[-1][-1]+1],axis=(0,1))
-beta_avg_power = np.mean(especs[:,cutf_beta[0][0]:cutf_beta[-1][-1] + 1,cutt[0][0]:cutt[-1][-1]+1],axis=(0,1))
+cutt,new_time = pass_range.pass_range(t,0,60) #指定した範囲で集計
+cutf_alpha,alpha_freqs= pass_range.pass_range(freqs,8,13)
+cutf_beta,beta_feqs= pass_range.pass_range(freqs,13,25)
+alpha_power = np.mean(especs[CH_IDX,cutf_alpha,:][:,cutt],axis=0)
+beta_power = np.mean(especs[CH_IDX,cutf_beta,:][:,cutt],axis=0)
+alpha_avg_power = np.mean(especs[:,cutf_alpha,:][:,:,cutt],axis=(0,1))
+beta_avg_power = np.mean(especs[:,cutf_beta,:][:,:,cutt],axis=(0,1))
 plt.title("avg ch : α,β,β/α")
 plt.plot(new_time,alpha_avg_power,label="α")
 plt.plot(new_time,beta_avg_power,label="β")
