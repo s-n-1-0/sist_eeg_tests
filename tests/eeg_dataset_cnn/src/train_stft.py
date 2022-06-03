11462a1188024390abe5036e7004bae1f70c8ea7
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2022-06-03 14:39:58 +0900

    eeg_dataset_cnnデータセットのsubj1_series2_data.csv(指定したチャンネル)をスペクトログラムでプロットする処理を追加

---
+++ tests/eeg_dataset_cnn/src/train_stft.py
@@ -0,0 +1,25 @@
+"""
+trainデータの特定チャネルをスペクトログラムでプロットする。
+(確認用)
+"""
+# %%
+from  edf_viewer import spec
+import pandas as pd
+import json
+import numpy as np
+import matplotlib.pyplot as plt
+with open("tests/eeg_dataset_cnn/src/settings.json","r") as json_file:
+    settings = json.load(json_file)
+dataset_path = settings["dataset_path"]
+csv = pd.read_csv(f"{dataset_path}/train/subj1_series2_data.csv")
+
+# %%
+channel = 1
+data = csv.values
+sig = np.array(data[:,channel],dtype=np.float32)
+freqs,t,sp = spec.get_spectrogram(sig,500)
+logsp = 10 * np.log10(sp)
+plt.figure()
+plt.pcolormesh(t,freqs,logsp, shading='auto')
+plt.show()
+# %%
