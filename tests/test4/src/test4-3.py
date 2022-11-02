21f2b5b6701a8104ac7fad6b8139adf7c84c7bdd
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2022-11-02 09:42:37 +0900

    test4-3.pyのコードを整理

--- tests/test4/src/test4-3.py
+++ tests/test4/src/test4-3.py
@@ -2,24 +2,24 @@
 import pyedflib
 from scipy import signal
 import edf_viewer as myedf
-EDF_PATH = "edf_files/test4/test4_3_0930_run.edf"
+EDF_PATH = "edf_files/test4/test4_3_0930.edf"
 
-# %%
+# %% lowpass
+# 引用 : https://mori-memo.hateblo.jp/entry/2022/04/30/235815
+def butter_lowpass(lowcut, fs, order=4):
+    '''バターワースローパスフィルタを設計する関数
+    '''
+    nyq = 0.5 * fs
+    low = lowcut / nyq
+    b, a = signal.butter(order, low, btype='low')
+    return b, a
+def butter_lowpass_filter(x, lowcut, fs, order=4):
+    '''データにローパスフィルタをかける関数
+    '''
+    b, a = butter_lowpass(lowcut, fs, order=order)
+    y = signal.filtfilt(b, a, x)
+    return y
 with pyedflib.EdfReader(EDF_PATH) as redf:
-    # 引用 : https://mori-memo.hateblo.jp/entry/2022/04/30/235815
-    def butter_lowpass(lowcut, fs, order=4):
-        '''バターワースローパスフィルタを設計する関数
-        '''
-        nyq = 0.5 * fs
-        low = lowcut / nyq
-        b, a = signal.butter(order, low, btype='low')
-        return b, a
-    def butter_lowpass_filter(x, lowcut, fs, order=4):
-        '''データにローパスフィルタをかける関数
-        '''
-        b, a = butter_lowpass(lowcut, fs, order=order)
-        y = signal.filtfilt(b, a, x)
-        return y
     fs = myedf.get_fs(redf)
     ch = myedf.get_channel_length(redf)
     with pyedflib.EdfWriter("./copy.edf",ch) as wedf:
