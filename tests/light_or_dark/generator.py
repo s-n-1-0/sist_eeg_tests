e04a0289017b0b2c6726d3cbd79f849f151707b2
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2022-11-13 00:13:09 +0900

    lord generatorの追加

---
+++ tests/light_or_dark/generator.py
@@ -0,0 +1,19 @@
+# %%
+from typing import Any, Callable
+import h5py
+import numpy as np
+def generator(path:str,signal_size:int,batch_size:int,label_func:Callable[[str],Any]):
+    with h5py.File(path, 'r') as hf:
+        group = hf["annotations/Marker"]
+        keys = list(group.keys())
+        steps = len(keys) // batch_size
+        count = 0
+        while count < steps:
+            x = []
+            y = []
+            for i in range(batch_size):
+                dataset = group[keys[i + count * batch_size]]
+                x.append(dataset[:,:signal_size])
+                y.append(label_func(dataset.attrs["label"]))
+            yield (np.array(x,dtype=np.float32).transpose(0,2,1),np.array(y,dtype=np.float32))
+            count += 1
