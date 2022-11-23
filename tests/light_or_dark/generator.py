43dd8a8f8297333ff7f80392be8b841708ff9333
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2022-11-24 00:13:20 +0900

    lord generatorのデータセット読み込み処理を整理

--- tests/light_or_dark/generator.py
+++ tests/light_or_dark/generator.py
@@ -3,7 +3,7 @@ import random
 from typing import Any, Callable
 import h5py
 import numpy as np
-def make_generators(path:str,batch_size:int,border:int,label_func:Callable[[str],Any],pick_func:Callable[[np.ndarray],np.ndarray]):
+def make_generators(path:str,batch_size:int,border:int,label_func:Callable[[str],Any],pick_func:Callable[[h5py.Dataset,bool],np.ndarray]):
     with h5py.File(path, 'r') as hf:
         group = hf["annotations/Marker"]
         origin_keys = list(group.keys())
@@ -18,17 +18,19 @@ def make_generators(path:str,batch_size:int,border:int,label_func:Callable[[str]
                     random.shuffle(keys)
                 else:
                     keys = all_keys[border:]
-                steps = len(keys) // batch_size
                 count = 0
-                while count < steps:
-                    x = []
-                    y = []
-                    for i in range(batch_size):
-                        dataset = group[keys[i + count * batch_size]]
-                        x.append(pick_func(dataset))
-                        y.append(label_func(dataset.attrs["label"]))
-                    yield (np.array(x,dtype=np.float32).transpose(0,2,1),np.array(y,dtype=np.float32))
+                x = []
+                y = []
+                while count < len(keys):
+                    dataset = group[keys[count]]
                     count += 1
+                    x.append(pick_func(dataset,mode))
+                    y.append(label_func(dataset.attrs["label"]))
+                    if count % batch_size == 0:
+                        yield (np.array(x,dtype=np.float32).transpose(0,2,1),np.array(y,dtype=np.float32))
+                        x = []
+                        y = []
+
         return generator
     return make_generator(True),make_generator(False)
 # %%
