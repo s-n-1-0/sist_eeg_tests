5730e50ca459816accd1bfbef89ed7c957debabc
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2022-11-27 16:31:43 +0900

    lord2 take6 追加

--- tests/light_or_dark2/take6_lstm.py
+++ tests/light_or_dark2/take6_lstm.py
@@ -1,39 +1,36 @@
 # %%
-import random
 import tensorflow as tf
 from keras.models import Sequential
-from keras.layers import Dense,  LSTM,Activation
+from keras.layers import Dense,  LSTM,Activation,Dropout
 import numpy as np
 from generator import make_generators
 from utils.history import save_history,plot_history
 # %% 
-# take1 Wall time: 20min 3s
-# take2,3 Wall time: 7min 14s
-back = 500 #take 2,3 = 160 # take1,4 = 500
+back = 350
 ch = 10
+batch_size = 4
 # %%
 model = Sequential()
-model.add(LSTM(100, 
-            activation='tanh', 
-            recurrent_activation='sigmoid'))
+model.add(LSTM(64,return_sequences=True,dropout=0.5))
+model.add(LSTM(32,dropout=0.5))
 model.add(Dense(1))
 model.add(Activation("sigmoid"))
 model.compile(loss='binary_crossentropy', 
-            optimizer='adam', 
+            optimizer=tf.optimizers.Adam(learning_rate=0.0001), 
             metrics=["binary_accuracy"])
 output_shapes=([None,back,ch], [None])
 
 def take6_pick(signal:np.ndarray,mode:bool):
     return signal[:,:back]
 
-tgen,vgen = make_generators("./dataset/lord2/ex.h5",4,-20,label_func=lambda label: int(label == "dark"),pick_func=take6_pick)
+tgen,vgen = make_generators("./dataset/lord2/ex.h5",batch_size,-100,label_func=lambda label: int(label == "dark"),pick_func=take6_pick)
 def from_generator(gen):
     return tf.data.Dataset.from_generator(gen,output_types=(np.float32,np.float32), output_shapes=output_shapes)
 tgen = from_generator(tgen)
 vgen = from_generator(vgen)
 history = model.fit(tgen,
-        epochs=500, 
-        batch_size=4,
+        epochs=50, 
+        batch_size=batch_size,
         validation_data= vgen)
 #predict = model.predict(test, verbose=1)
 # %%
