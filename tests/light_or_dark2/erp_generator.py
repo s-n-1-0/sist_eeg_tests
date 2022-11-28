2d576fb449617e09265e010d4b0461fa444281ff
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2022-11-29 01:28:27 +0900

    lord2 datasetにerpを混ぜるジェネレーターの仮追加

---
+++ tests/light_or_dark2/erp_generator.py
@@ -0,0 +1,69 @@
+# %%
+import random
+from typing import Any, Callable
+import h5py
+import numpy as np
+rate = 0.6
+from utils.signal import norm
+def make_generators(path:str,erp_size:int,batch_size:int,border:int,label_func:Callable[[str],Any],pick_func:Callable[[h5py.Dataset,bool],np.ndarray]):
+    with h5py.File(path, 'r') as hf:
+        group = hf["annotations/Marker"]
+        origin_keys = list(group.keys())
+        all_keys = origin_keys[:]
+        random.shuffle(all_keys)
+        train_all_keys  = all_keys[:border]
+        train_dark_keys = []
+        train_light_keys = []
+        for key in train_all_keys:
+            if group[key].attrs["label"] == "dark":
+                train_dark_keys.append(key)
+            else:
+                train_light_keys.append(key)
+    def make_train_generator():
+        with h5py.File(path, 'r') as hf:
+            group = hf["annotations/Marker"]
+            def get_dataset(keys:list[str]):
+                random.shuffle(keys)
+                if random.random() >= rate:
+                    erp = []
+                    for i in range(erp_size):
+                        dataset = group[keys[i]]
+                        erp.append(pick_func(dataset,False))
+                    return norm(np.array(erp).sum(axis=0) / erp_size)#norm(norm(np.array(erp).sum(axis=0) / erp_size) * 0.2 + pick_func(group[keys[erp_size + 1]],False)* 0.8)
+                else:
+                    return pick_func(group[keys[0]],False)
+            itr_count = 0
+            batch_count = 0
+            steps = len(train_all_keys) // batch_size
+            x = []
+            y = []
+            while itr_count < steps:
+                batch_count += 1
+                is_dark = random.random() >= 0.5
+                x.append(get_dataset(train_dark_keys if is_dark else train_light_keys))
+                y.append(label_func("dark" if is_dark else "light"))
+                
+                if batch_count % batch_size == 0:
+                    yield (np.array(x,dtype=np.float32).transpose(0,2,1),np.array(y,dtype=np.float32))
+                    x = []
+                    y = []
+                    itr_count += 1 
+    def make_valid_generator():
+        with h5py.File(path, 'r') as hf:
+            group = hf["annotations/Marker"]
+            keys = all_keys[border:]
+            count = 0
+            x = []
+            y = []
+            while count < len(keys):
+                dataset = group[keys[count]]
+                count += 1
+                x.append(pick_func(dataset,False))
+                y.append(label_func(dataset.attrs["label"]))
+                if count % batch_size == 0:
+                    yield (np.array(x,dtype=np.float32).transpose(0,2,1),np.array(y,dtype=np.float32))
+                    x = []
+                    y = []
+
+    return make_train_generator,make_valid_generator
+# %%
--- tests/light_or_dark2/take6_lstm.py
+++ tests/light_or_dark2/take6_lstm.py
@@ -3,10 +3,10 @@ import tensorflow as tf
 from keras.models import Sequential
 from keras.layers import Dense,  LSTM,Activation,Dropout
 import numpy as np
-from generator import make_generators
+from erp_generator import make_generators
 from utils.history import save_history,plot_history
 # %% 
-back = 350
+back = 100
 ch = 10
 batch_size = 4
 # %%
@@ -16,20 +16,20 @@ model.add(LSTM(32,dropout=0.5))
 model.add(Dense(1))
 model.add(Activation("sigmoid"))
 model.compile(loss='binary_crossentropy', 
-            optimizer=tf.optimizers.Adam(learning_rate=0.0001), 
+            optimizer=tf.optimizers.Adam(learning_rate=0.00001), #0.000001
             metrics=["binary_accuracy"])
 output_shapes=([None,back,ch], [None])
 
 def take6_pick(signal:np.ndarray,mode:bool):
-    return signal[:,:back]
+    return signal[:,50:50 + back]
 
-tgen,vgen = make_generators("./dataset/lord2/ex.h5",batch_size,-100,label_func=lambda label: int(label == "dark"),pick_func=take6_pick)
+tgen,vgen = make_generators("./dataset/lord2/ex.h5",100,batch_size,-100,label_func=lambda label: int(label == "dark"),pick_func=take6_pick)
 def from_generator(gen):
     return tf.data.Dataset.from_generator(gen,output_types=(np.float32,np.float32), output_shapes=output_shapes)
 tgen = from_generator(tgen)
 vgen = from_generator(vgen)
 history = model.fit(tgen,
-        epochs=50, 
+        epochs=20, 
         batch_size=batch_size,
         validation_data= vgen)
 #predict = model.predict(test, verbose=1)
