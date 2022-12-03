2dc712ba116338600effaacb2a0fd3ad2055ddd3
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2022-12-04 01:47:03 +0900

    lord2 2dcnnモデルを追加

--- tests/light_or_dark2/generators/generator.py
+++ tests/light_or_dark2/generators/generator.py
@@ -3,7 +3,12 @@ import random
 from typing import Any, Callable
 import h5py
 import numpy as np
-def make_generators(path:str,batch_size:int,border:int,label_func:Callable[[str],Any],pick_func:Callable[[h5py.Dataset,bool],np.ndarray]):
+def make_generators(path:str,
+                    batch_size:int,
+                    border:int,
+                    label_func:Callable[[str],Any],
+                    pick_func:Callable[[h5py.Dataset,bool],np.ndarray],
+                    transpose_rule = [0,2,1]):
     with h5py.File(path, 'r') as hf:
         group = hf["annotations/Marker"]
         origin_keys = list(group.keys())
@@ -27,7 +32,7 @@ def make_generators(path:str,batch_size:int,border:int,label_func:Callable[[str]
                     x.append(pick_func(dataset,mode))
                     y.append(label_func(dataset.attrs["label"]))
                     if count % batch_size == 0:
-                        yield (np.array(x,dtype=np.float32).transpose(0,2,1),np.array(y,dtype=np.float32))
+                        yield (np.array(x,dtype=np.float32).transpose(*transpose_rule),np.array(y,dtype=np.float32))
                         x = []
                         y = []
 
