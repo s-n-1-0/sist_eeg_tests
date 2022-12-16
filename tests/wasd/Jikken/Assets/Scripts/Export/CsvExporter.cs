714ff6b725db7cce9fc1ff332df2d58a16e3ad7e
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2022-12-16 15:42:26 +0900

    wasd CSV出力クラスを追加

---
+++ tests/wasd/Jikken/Assets/Scripts/Export/CsvExporter.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using UnityEngine;
+
+public class CsvExporter
+{
+    private StreamWriter sw;
+    private System.Diagnostics.Stopwatch totalStopwatch = new System.Diagnostics.Stopwatch();
+    private System.Diagnostics.Stopwatch syncStopwatch = new System.Diagnostics.Stopwatch();
+    public CsvExporter()
+    {
+        sw = new StreamWriter($"./export_{DateTime.Now.ToString().Replace("/","_").Replace(" ","_").Replace(":","")}.csv", true, Encoding.UTF8);
+        string[] headers = { "Annotation","Total Time", "Sync Elapsed Time" };
+        sw.WriteLine(string.Join(",", headers));
+        totalStopwatch.Start();
+    }
+
+    public void Record(string ann)
+    {
+        if (ann == "sync") syncStopwatch.Restart();
+        string[] s1 = { ann, totalStopwatch.Elapsed.TotalSeconds.ToString(), syncStopwatch.Elapsed.TotalSeconds.ToString() };
+        string s2 = string.Join(",", s1);
+        sw.WriteLine(s2);
+    }
+    public void Close()
+    {
+        sw.Close();
+    }
+}
