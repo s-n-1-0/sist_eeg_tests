f6ad018a6fe46728fd980940d44839ad0e377210
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2023-01-09 23:29:45 +0900

    lord3 抽出後データセットに待機画像表示時のデータを追加

--- tests/light_or_dark2/preprocessing.py
+++ tests/light_or_dark2/preprocessing.py
@@ -4,7 +4,7 @@ from labedf import csv2,edf2
 import numpy as np
 from scipy import signal
 from pyedflib import EdfReader
-from utils import edf as myedf,edflab as myedflab,signals_standardization
+from utils import edf as myedf,edflab as myedflab,signals_standardization,lowpass_filter
 DATASET_DIR_PATH = "./dataset/lord2/train"
 file_settings = myedflab.MergeAllCsv2EdfFileSettings(DATASET_DIR_PATH + "/ペア.csv",list_encoding="ansi")
 edfcsv_filenames = file_settings.get_edfcsv_filenames()
@@ -12,26 +12,12 @@ with EdfReader(f"{DATASET_DIR_PATH}/edf/{edfcsv_filenames[0,0]}") as er:
     fs = int(myedf.get_fs(er))
 
 # %% merge csv,edf
-filenames = myedflab.merge_all_csv2edf(file_settings,label_header_name="LorD")
+filenames = myedflab.merge_all_csv2edf(file_settings,label_header_name="LorD",marker_names=["Marker","Wait"])
 filenames
 # %% to hdf
 export_path = f"{DATASET_DIR_PATH}/ex.h5"
-# 引用 : https://mori-memo.hateblo.jp/entry/2022/04/30/235815
-def butter_lowpass(lowcut, order=4):
-    '''バターワースローパスフィルタを設計する関数
-    '''
-    nyq = 0.5 * fs
-    low = lowcut / nyq
-    b, a = signal.butter(order, low, btype='low')
-    return b, a
-def butter_lowpass_filter(x, lowcut,order=4):
-    '''データにローパスフィルタをかける関数
-    '''
-    b, a = butter_lowpass(lowcut,order=order)
-    y = signal.filtfilt(b, a, x)
-    return y
 def before_preprocessing(signals:list[np.ndarray]):
-    signals = [butter_lowpass_filter(signal,30) for signal in signals]
+    signals = [lowpass_filter(fs,signal,30) for signal in signals]
     return signals
 def after_preprocessing(signals:np.ndarray,label:str):
     if label != "dark" and label != "light":
