daa1841285af61370376c07168d5778c32fd98bb
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2023-01-23 16:13:15 +0900

    pre_eeglab 複数のedfファイル処理対応

--- notes/preprocessing/pre_eeglab.m
+++ notes/preprocessing/pre_eeglab.m
@@ -1,22 +1,32 @@
 %% 
-eeg = pop_biosig({'./ex.edf'});
-%%フィルタリング
-eeg = pop_eegfiltnew(eeg,1,[]);
-eeg = pop_eegfiltnew(eeg,[],40);
-eeg = pop_epoch(eeg,{"Marker__dark" "Marker__light"},[-1 2]); %#ok<CLARRSTR> 
-disp("--")
-is_after_reject = 0;%0だとディスプレイ表示のリジェクトする前と母数が一致
-eeg = pop_eegthresh(eeg,1,[1:10],-200,200,-1,1.998,0,is_after_reject,0); %"Find abnormal values" debug : 200
-eeg = pop_rejtrend(eeg,1,[1:10],1500,0.5,0.3,0,is_after_reject,0);
-eeg = pop_jointprob(eeg,1,[1:10],5,5,0,is_after_reject,0,0,0);
-eeg = pop_rejkurt(eeg,1,[1:10],5,5,0,is_after_reject,0,0,0);
-eeg = pop_rejspec(eeg,1,"threshold",[-60 40],"freqlimits",[0,40],"eegplotreject",is_after_reject);
-if ~is_after_reject
-    eeg = eeg_rejsuperpose(eeg, 1,1,1,1,1,1,1,1);
-    eeg = pop_rejepoch(eeg,eeg.reject.rejglobal,0);
-end
+filelst = dir("..\..\dataset\lord2\train\build\*.edf");
+sz = size(filelst);
+disp(sz(1));
 %% 
-pop_saveset(eeg,"filename",'./ex.set');
+for i = 1:sz
+    f = filelst(i);
+    preprocessing_eeg(f.folder + "\" + f.name,"..\..\dataset\lord2\train\pre")
+end
+function preprocessing_eeg(full_filepath,export_dir_path)
+    [filepath,filename,ext] = fileparts(full_filepath);
+    eeg = pop_biosig({convertStringsToChars(filepath + "\" + filename + ext)});
+    %%フィルタリング
+    eeg = pop_eegfiltnew(eeg,1,[]);
+    eeg = pop_eegfiltnew(eeg,[],40);
+    eeg = pop_epoch(eeg,{"Marker__dark" "Marker__light"},[-1 2]); %#ok<CLARRSTR> 
+    disp("--")
+    is_after_reject = 0;%0だとディスプレイ表示のリジェクトする前と母数が一致
+    eeg = pop_eegthresh(eeg,1,[1:10],-200,200,-1,1.998,0,is_after_reject,0); %"Find abnormal values" debug : 200
+    eeg = pop_rejtrend(eeg,1,[1:10],1500,0.5,0.3,0,is_after_reject,0);
+    eeg = pop_jointprob(eeg,1,[1:10],5,5,0,is_after_reject,0,0,0);
+    eeg = pop_rejkurt(eeg,1,[1:10],5,5,0,is_after_reject,0,0,0);
+    eeg = pop_rejspec(eeg,1,"threshold",[-60 40],"freqlimits",[0,40],"eegplotreject",is_after_reject);
+    if ~is_after_reject
+        eeg = eeg_rejsuperpose(eeg, 1,1,1,1,1,1,1,1);
+        eeg = pop_rejepoch(eeg,eeg.reject.rejglobal,0);
+    end
+    pop_saveset(eeg,"filename",convertStringsToChars(export_dir_path + "\" + filename + ".set"));
+end
 
 %pop_editset(eeg)
 %eegplot(eeg.data,'srate',500) %srate = fs
