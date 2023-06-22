675263eeb387c481c7efbf73dc10fdb55c583a82
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2023-06-22 20:05:45 +0900

    とりあえずエポック分割まで動いた(要整理)

--- tests/3_paradigms/preprocessing.m
+++ tests/3_paradigms/preprocessing.m
@@ -1,5 +1,6 @@
 %% 
 filelst = dir("MIOnly_FTP_EEG Dataset and OpenBMI Toolbox for Three BCI Paradigms\session1");
+fs = 1000;
 filelst = filelst(endsWith({filelst.name},".mat"));
 sz = size(filelst);
 disp(sz(1));
@@ -7,30 +8,44 @@ disp(sz(1));
 %%
 fp = filelst(2);
 data = extract_data(fp.folder+"/"+fp.name);
+%%
+preprocessing_eeg(fp.folder + "/pre/"+fp.name + ".set",data{1},fs)
 %% 
 for i = 1:sz
     f = filelst(i);
-    preprocessing_eeg(f.folder + "\" + f.name,".\pre")
+    preprocessing_eeg(f.folder + "\" + f.name,".\pre");
 end
-function preprocessing_eeg(full_filepath,export_dir_path)
-    [filepath,filename,ext] = fileparts(full_filepath);
-    eeg = pop_biosig({convertStringsToChars(filepath + "\" + filename + ext)});
+function[t] = preprocessing_eeg(export_dir_path,data,fs)
+    eegdata = data{1}.';%%ch × samples
+    eegindexes = data{2};
+
+    eeg = pop_importdata('dataformat', 'array', 'data', eegdata, 'setname', 'EEG', 'srate', fs);
+    % イベントを追加する
+    eeg = eeg_addnewevents(eeg,{eegindexes},{'x'});
+    eeg.event = rmfield(eeg.event,"duration");
+    eeg.urevent = rmfield(eeg.urevent,"duration");
+    % イベント構造を更新する
+    %eeg = eeg_checkset(eeg);
     %%フィルタリング
     eeg = pop_eegfiltnew(eeg,1,[]);
     eeg = pop_eegfiltnew(eeg,[],30);
-    eeg = pop_epoch(eeg,{"Marker__dark" "Marker__light"},[-1 2]); %#ok<CLARRSTR> 
+    eeg = pop_epoch(eeg)%(eeg, {"boundary"}, [0, 1]);
+    t = eeg;
+    disp(eeg)
     disp("--")
     is_after_reject = 0;%0だとディスプレイ表示のリジェクトする前と母数が一致
-    eeg = pop_eegthresh(eeg,1,[1:10],-100,100,-1,1.998,0,is_after_reject,0); %"Find abnormal values" debug : 200
-    eeg = pop_rejtrend(eeg,1,[1:10],1500,0.5,0.3,0,is_after_reject,0);
-    eeg = pop_jointprob(eeg,1,[1:10],5,5,0,is_after_reject,0,0,0);
-    eeg = pop_rejkurt(eeg,1,[1:10],5,5,0,is_after_reject,0,0,0);
+    eeg = pop_eegthresh(eeg,1,[1:62],-100,100,-1,1.998,0,is_after_reject,0); %"Find abnormal values" debug : 200
+    eeg = pop_rejtrend(eeg,1,[1:62],1500,0.5,0.3,0,is_after_reject,0);
+    eeg = pop_jointprob(eeg,1,[1:62],5,5,0,is_after_reject,0,0,0);
+    eeg = pop_rejkurt(eeg,1,[1:62],5,5,0,is_after_reject,0,0,0);
     eeg = pop_rejspec(eeg,1,"threshold",[-60 40],"freqlimits",[0,40],"eegplotreject",is_after_reject);
     if ~is_after_reject
+        %disp(eeg.reject)
         eeg = eeg_rejsuperpose(eeg, 1,1,1,1,1,1,1,1);
+        %disp(eeg.reject)
         eeg = pop_rejepoch(eeg,eeg.reject.rejglobal,0);
     end
-    pop_saveset(eeg,"filename",convertStringsToChars(export_dir_path + "\" + filename + ".set"));
+    pop_saveset(eeg,"filename",convertStringsToChars(export_dir_path));
 end
 
 %%
