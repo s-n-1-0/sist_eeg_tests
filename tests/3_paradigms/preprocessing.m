ff6d2e182c53ee87e5ea9a6032d227b6318cb53d
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2023-06-23 14:07:10 +0900

    3p 最低限必要な処理の追加と整理

--- tests/3_paradigms/preprocessing.m
+++ tests/3_paradigms/preprocessing.m
@@ -5,44 +5,39 @@ filelst = filelst(endsWith({filelst.name},".mat"));
 sz = size(filelst);
 disp(sz(1));
 
-%%
+%% preview
 fp = filelst(2);
 data = extract_data(fp.folder+"/"+fp.name);
-%%
 preprocessing_eeg(fp.folder + "/pre/"+fp.name + ".set",data{1},fs)
 %% 
 for i = 1:sz
     f = filelst(i);
     preprocessing_eeg(f.folder + "\" + f.name,".\pre");
 end
-function[t] = preprocessing_eeg(export_dir_path,data,fs)
+function preprocessing_eeg(export_dir_path,data,fs)
     eegdata = data{1}.';%%ch × samples
     eegindexes = data{2};
 
     eeg = pop_importdata('dataformat', 'array', 'data', eegdata, 'setname', 'EEG', 'srate', fs);
     % イベントを追加する
     eeg = eeg_addnewevents(eeg,{eegindexes},{'x'});
-    eeg.event = rmfield(eeg.event,"duration");
-    eeg.urevent = rmfield(eeg.urevent,"duration");
     % イベント構造を更新する
     %eeg = eeg_checkset(eeg);
+    %%ダウンサンプリング
+    eeg = pop_resample(eeg,250);
     %%フィルタリング
     eeg = pop_eegfiltnew(eeg,1,[]);
     eeg = pop_eegfiltnew(eeg,[],30);
-    eeg = pop_epoch(eeg)%(eeg, {"boundary"}, [0, 1]);
-    t = eeg;
-    disp(eeg)
-    disp("--")
+    eeg = pop_epoch(eeg, {"x"}, [0, 4]);
     is_after_reject = 0;%0だとディスプレイ表示のリジェクトする前と母数が一致
-    eeg = pop_eegthresh(eeg,1,[1:62],-100,100,-1,1.998,0,is_after_reject,0); %"Find abnormal values" debug : 200
+    %TODO:一時的に範囲を上げてる
+    eeg = pop_eegthresh(eeg,1,[1:62],-500,500,-1,1.998,0,is_after_reject,0); %"Find abnormal values" default : 100
     eeg = pop_rejtrend(eeg,1,[1:62],1500,0.5,0.3,0,is_after_reject,0);
     eeg = pop_jointprob(eeg,1,[1:62],5,5,0,is_after_reject,0,0,0);
     eeg = pop_rejkurt(eeg,1,[1:62],5,5,0,is_after_reject,0,0,0);
     eeg = pop_rejspec(eeg,1,"threshold",[-60 40],"freqlimits",[0,40],"eegplotreject",is_after_reject);
     if ~is_after_reject
-        %disp(eeg.reject)
         eeg = eeg_rejsuperpose(eeg, 1,1,1,1,1,1,1,1);
-        %disp(eeg.reject)
         eeg = pop_rejepoch(eeg,eeg.reject.rejglobal,0);
     end
     pop_saveset(eeg,"filename",convertStringsToChars(export_dir_path));
