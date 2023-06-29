d8bdd395f581aedd628ff454677d2f573bc20f37
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2023-06-29 19:01:08 +0900

    3p 1DCNN-PSDパターンファイルを追加(仮)

---
+++ tests/3_paradigms/1dcnn_psd.py
@@ -0,0 +1,70 @@
+# %%
+import tensorflow as tf
+from keras.models import Sequential
+from keras.callbacks import ReduceLROnPlateau
+from keras.layers import Dense,Activation,Dropout,Conv1D,MaxPooling1D,Flatten,BatchNormalization
+import numpy as np
+from generator import PsdGeneratorMaker,dataset_dir_path
+from summary import summary
+
+
+# %% 
+back = 50
+ch = 3
+batch_size = 32
+# %%
+model = Sequential()
+model.add(Conv1D(
+            filters=32,
+            kernel_size= 10,
+        ))
+model.add(BatchNormalization())
+model.add(Activation("relu"))
+model.add(Dropout(0.4))
+model.add(Conv1D(
+            filters=64,
+            kernel_size= 7,
+            padding='same'
+        ))
+model.add(BatchNormalization())
+model.add(Activation("relu"))
+model.add(Dropout(0.4))
+model.add(MaxPooling1D(
+            pool_size=3,
+            strides=2,
+            padding="same"
+        ))
+model.add(Flatten())
+model.add(Dense(128,activation="sigmoid"))
+model.add(Dropout(0.4))
+model.add(Dense(1,activation="sigmoid"))
+model.compile(loss='binary_crossentropy', 
+            optimizer=tf.optimizers.Adam(learning_rate=0.001), #0.000001
+            metrics=["binary_accuracy"])
+output_shapes=([None,back,ch], [None])
+
+def pick_func(signal:np.ndarray,mode:bool):
+    return signal[12:15,31:81]
+maker = PsdGeneratorMaker(f"{dataset_dir_path}/3pdataset.h5")
+tgen,vgen = maker.make_generators(batch_size,pick_func=pick_func)
+def from_generator(gen):
+    return tf.data.Dataset.from_generator(gen,output_types=(np.float32,np.float32), output_shapes=output_shapes)
+tgen = from_generator(tgen)
+vgen = from_generator(vgen)
+model.build(output_shapes[0])
+model.summary()
+# %%
+reduce_lr = ReduceLROnPlateau(
+                        monitor='val_loss',
+                        factor=0.5,
+                        patience=4,
+                        min_lr=0.00001
+                )
+history = model.fit(tgen,
+        epochs=3, 
+        batch_size=batch_size,
+        validation_data= vgen,
+        callbacks=[reduce_lr])
+# %%
+summary(model,history,vgen)
+# %%
