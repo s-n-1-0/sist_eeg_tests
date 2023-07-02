2943232759e2167474f7eaabd28c8dd1a3531b54
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2023-07-03 01:10:41 +0900

    3p 使いまわしているpick_func関数を別モジュールへ移動

---
+++ tests/3_paradigms/picker.py
@@ -0,0 +1,23 @@
+import h5py
+import random
+class BasePicker():
+    def __init__(self) -> None:
+        self.ch_list = [12, 13, 14, 35, 36, 8, 7, 9, 10, 18, 17, 19, 20]
+
+class RawPicker(BasePicker):
+    def __init__(self,sample_size:int) -> None:
+        super().__init__()
+        self.sample_size = sample_size
+    def make_pick_func(self,offset = 0):
+        def pick_func(signal: h5py.Dataset, _: bool):
+            return signal[()][self.ch_list,offset:self.sample_size+offset]
+        return pick_func
+    
+    #ランダムの時点を開始地点としてサンプルをとる
+    def make_random_pick_func(self,max_sample_size:int):
+        def pick_func(signal: h5py.Dataset, _: bool):
+            random_offset = random.randint(0,max_sample_size-self.sample_size)
+            return signal[()][self.ch_list,random_offset:self.sample_size+random_offset]
+        return pick_func
+        
+
