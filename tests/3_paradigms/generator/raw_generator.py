a8fe38d2ee20cbce45aaff2028852f92b0e2644c
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2023-08-13 23:13:25 +0900

    RawGeneratorMakerのtranspose関数を外部から読み取れるように修正

--- tests/3_paradigms/generator/raw_generator.py
+++ tests/3_paradigms/generator/raw_generator.py
@@ -4,17 +4,17 @@ import random
 from typing import Any, Callable
 import h5py
 import numpy as np
-
+def transpose_raw1d(_:int,batch:np.ndarray):
+    return batch.transpose([0,2,1])
+def transpose_raw2d(_:int,batch:np.ndarray):
+    return batch[:,:,:,None]#.transpose([0,2,1])
 class RawGeneratorMaker(BaseGeneratorMaker):
     def __init__(self, hdf_path: str,istest=False) -> None:
         group_name = "std"
         super().__init__(hdf_path, group_name,istest=istest)
+
     def make_generators(self, batch_size: int,pick_func: Callable[[h5py.Dataset, bool], np.ndarray]):
-        def transpose_func(_:int,batch:np.ndarray):
-             return batch.transpose([0,2,1])
-        return super().make_generators(batch_size,pick_func,transpose_func=transpose_func)
+        return super().make_generators(batch_size,pick_func,transpose_func=transpose_raw1d)
     def make_2d_generators(self, batch_size: int,pick_func: Callable[[h5py.Dataset, bool], np.ndarray]):
-        def transpose_func(_:int,batch:np.ndarray):
-            return batch[:,:,:,None]#.transpose([0,2,1])
-        return super().make_generators(batch_size,pick_func,transpose_func=transpose_func)
+        return super().make_generators(batch_size,pick_func,transpose_func=transpose_raw2d)
 # %%
