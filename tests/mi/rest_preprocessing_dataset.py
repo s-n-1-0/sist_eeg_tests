b5fcfb5e963ce0a41f4e680abdff3bcae4ca7fef
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2023-09-15 13:13:14 +0900

    Mi52データセットのRestデータの処理を追加

--- tests/mi/rest_preprocessing_dataset.py
+++ tests/mi/rest_preprocessing_dataset.py
@@ -26,9 +26,8 @@ def downsample_multichannel_signal(input_signal, original_rate, target_rate):
         # 出力信号に格納
         downsampled_signal[channel, :] = downsampled_channel_signal
     return downsampled_signal
-dataset_size = (54,54)
 train_key = "EEG_MI_train"
-def parse_mat(parent_key,mat_data):
+def p3_parse_mat(parent_key,mat_data):
     data = mat_data[parent_key][0][0]
     #形式整理
     for i in range(len(data)):
@@ -40,31 +39,31 @@ def parse_mat(parent_key,mat_data):
     return data
 def convert_average_spectrum(data):
     frequencies, times, Sxx = spectrogram(data, fs, nperseg=1024)
-    # 指定した周波数帯（10Hzから30Hz）にフィルタリング
-    low_freq = 10
+    # 指定した周波数帯（8Hzから30Hz）にフィルタリング
+    low_freq = 8
     high_freq = 30
     mask = (frequencies >= low_freq) & (frequencies <= high_freq)
     filtered_Sxx = Sxx[mask, :]
     # 時間方向に平均
     average_spectrum = np.mean(filtered_Sxx, axis=1)
     return average_spectrum
-session = scipy.io.loadmat(f"{root_path+p3_dir}/session1/sess{(1):02}_subj{(1):02}_EEG_MI.mat")
-train_rest = downsample_multichannel_signal(parse_mat(train_key,session)[13].T,1000,500)
+session = scipy.io.loadmat(f"{root_path+mi52_dir}/s{(1):02}.mat")
+train_rest = downsample_multichannel_signal(session["eeg"][0][0][1],512,500)
 train_rest =  StandardScaler().fit_transform(train_rest.T).T
-train_rest = train_rest[[7, 8, 9, 10, 12, 35, 13, 36, 14, 17, 18, 19, 20],:]
+train_rest = train_rest[[i - 1 for i in [9,11,46,44,13,12,48,49,50,17,19,56,54]],:]
 _train_rest = []
 for k in range(train_rest.shape[0]):
     _train_rest.append(convert_average_spectrum(train_rest[k,:]))
 np.array(_train_rest).shape
 #%% restデータ統合
-with h5py.File(root_path+"/test.h5",mode="w") as h5:
+with h5py.File(root_path+"/rest.h5",mode="w") as h5:
     if "rest" in h5:
         del h5["rest"]
     rest_group = h5.require_group("rest")
-    for i, size in enumerate(dataset_size):
+    for i, size in enumerate((54,54)):
         for j in range(size):
             session = scipy.io.loadmat(f"{root_path+p3_dir}/session{i+1}/sess{(i+1):02}_subj{(j+1):02}_EEG_MI.mat")
-            train_rest = downsample_multichannel_signal(parse_mat(train_key,session)[13].T,1000,500)
+            train_rest = downsample_multichannel_signal(p3_parse_mat(train_key,session)[13].T,1000,500)
             train_rest =  StandardScaler().fit_transform(train_rest.T).T
             train_rest = train_rest[[7, 8, 9, 10, 12, 35, 13, 36, 14, 17, 18, 19, 20],:]
             _train_rest = []
@@ -73,5 +72,21 @@ with h5py.File(root_path+"/test.h5",mode="w") as h5:
             train_rest = np.array(_train_rest)
             session_group = rest_group.require_group("3p").require_group(str(i+1))
             session_group.create_dataset(str(j+1),train_rest.shape,data=train_rest)
+    i = 0
+    size = 52
+    rest_group = h5.require_group("rest")
+    for j in range(size):
+        session = scipy.io.loadmat(f"{root_path+mi52_dir}/s{(i+1):02}.mat")
+        train_rest = downsample_multichannel_signal(session["eeg"][0][0][1],512,500)
+        train_rest =  StandardScaler().fit_transform(train_rest.T).T
+        train_rest = train_rest[[i - 1 for i in [9,11,46,44,13,12,48,49,50,17,19,56,54]],:]
+        _train_rest = []
+        for k in range(train_rest.shape[0]):
+            _train_rest.append(convert_average_spectrum(train_rest[k,:]))
+        train_rest = np.array(_train_rest)
+        session_group = rest_group.require_group("mi52").require_group(str(i+1))
+        session_group.create_dataset(str(j+1),train_rest.shape,data=train_rest)
+
+
 
 # %%
