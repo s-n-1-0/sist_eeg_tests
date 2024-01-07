c3219fa27ab381e59171eee5ca78a5dd539d609e
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2024-01-07 17:03:34 +0900

    predictにCSPLDAの設定を追加

--- tests/mi/predict.py
+++ tests/mi/predict.py
@@ -6,6 +6,7 @@ from sklearn.metrics import confusion_matrix
 from generator import RawGeneratorMaker,dataset_dir_path
 from pickfunc import RawPickFuncMaker
 from metrics import specificity
+from joblib import load
 csv_path = "./saves/mla_valid.csv"
 valids = list(pd.read_csv(csv_path,header=None).to_numpy()[0,:])
 sample_size = 750
@@ -15,20 +16,33 @@ maker = RawGeneratorMaker(f"{dataset_dir_path}/merged_mla2.h5",valid_keys=[str(k
 # %% 1DCNN
 model_path = "C:/_models/dec2/model-23.h5"
 _,vgen = maker.make_generators(batch_size,pick_func=pfm.make_random_pick_func())
+mode = "dl"
 # %% EEGNET
 model_path = "C:/_models/dec1/model-85.h5"
 _,vgen = maker.make_2d_generators(batch_size,pick_func=pfm.make_random_pick_func())
+mode ="dl"
+# %% CSP LDA
+model_path = "./cspldf.joblib"
+_,vgen = maker.make_generators(None,pick_func=pfm.make_random_pick_func())
+mode ="csplda"
 # %%
-loaded = keras.models.load_model(model_path,custom_objects={"specificity":specificity})
-loaded
+if mode == "dl":
+    loaded = keras.models.load_model(model_path,custom_objects={"specificity":specificity})
+elif mode == "csplda":
+    loaded = load(model_path) 
 n = 100
 acc_list = []
 for i in range(n):
     y_pred = []
     y_true = []
-    for data in  vgen():
-        Y_pred = loaded.predict(data[0])
-        y_pred += [1 if y > 0.5 else 0 for y in Y_pred ]
+    for data in  vgen():    
+        x = data[0]
+        if mode == "dl":
+            Y_pred = loaded.predict(x)
+            y_pred += [1 if y > 0.5 else 0 for y in Y_pred ]
+        elif mode == "csplda":
+            Y_pred = loaded.predict(x.transpose([0,2,1]))
+            y_pred += list(Y_pred)
         y_true += list(data[1])
     cm = confusion_matrix(y_true, y_pred)
     acc = (cm[0,0]+cm[1,1])/np.sum(cm)
@@ -49,4 +63,6 @@ np.mean(acc_list),np.std(acc_list)
 #   A:(0.6448586309523808, 0.010756414672879515)
 #   B:(0.6469568452380954, 0.01009357295394528)
 #   C:(0.6586681547619048, 0.009123234475514141)
+# CSPLDA:
+#   (0.5309558823529412, 0.0077958516384142315)
 # %%
